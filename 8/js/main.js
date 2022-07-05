d55d6f08e6d5c38523ac6c7ee1d623add49b800b
Keks <keks@htmlacademy.ru>
2022-07-05 08:48:31 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,4 @@
+import './miniature.js';
+import './bigpicture.js';
+import './form.js';
+import './validation.js';
---
+++ 8/js/miniature.js
@@ -0,0 +1,22 @@
+import { listDescriptionPhoto } from './data.js';
+
+const pictures = document.querySelector('.pictures');
+const template = document.querySelector('#picture').content;
+const templatePicture = template.querySelector('.picture');
+const fragmentPicture = document.createDocumentFragment();
+
+const MAX_PHOTO = 25;
+const newPhotos = listDescriptionPhoto(MAX_PHOTO);
+
+newPhotos.forEach(({ id, url, likes, comments }) => {
+  const photoElement = templatePicture.cloneNode(true);
+  photoElement.querySelector('.picture__img').src = url;
+  photoElement.querySelector('.picture__likes').textContent = likes;
+  photoElement.querySelector('.picture__comments').textContent = comments.length;
+  photoElement.setAttribute('data-id', id);
+  fragmentPicture.appendChild(photoElement);
+});
+
+pictures.appendChild(fragmentPicture);
+
+export { newPhotos };
