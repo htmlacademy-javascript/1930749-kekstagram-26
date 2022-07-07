134e258c1f5db4c4c93392f3d83ee4ecb74aca08
Ekaterina <ektregubova@gmail.com>
2022-07-07 17:58:20 +0500

    add scale and effects

--- js/validation.js
+++ js/validation.js
@@ -1,22 +1,28 @@
-import { imgUploadForm , textHashtags , textDescription } from './form.js';
 import { checkTextLength } from './util.js';
+
+const imgUploadForm = document.querySelector('.img-upload__form');
+const textHashtags = imgUploadForm.querySelector('.text__hashtags');
+const textDescription = imgUploadForm.querySelector('.text__description');
+
 const RE = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 const MAX_LENGTH_HASHTAG = 20;
 const MAX_HASHTAGS = 5;
 const MAX_LENGTH_COMMENT = 140;
 
 const pristine = new Pristine(imgUploadForm, {
-  classTo: 'img-upload__text',
-  errorTextParent: 'img-upload__text',
-  errorTextClass: 'img-upload__text--error',
+  classTo: 'text__element',
+  errorTextParent: 'text__element',
+  errorTextClass: 'text__element--error',
 });
+
+const rowIsEmpty = (hashtag) => hashtag.trim() === '';
 
 const getHashtagsArray = (hashtag) => hashtag.trim().split(' ');
 
-const validateHashTag = (hashtag) => getHashtagsArray(hashtag).every((textHashtag) => RE.test(textHashtag));
+const validateHashTag = (hashtag) => rowIsEmpty(hashtag) || getHashtagsArray(hashtag).every((textHashtag) => RE.test(textHashtag));
 pristine.addValidator(textHashtags, validateHashTag, 'Хэш-тег должен состоять из букв и чисел.');
 
-const checkHashSign = (hashtag) => getHashtagsArray(hashtag).every((textHashtag) => textHashtag[0] === '#');
+const checkHashSign = (hashtag) =>  rowIsEmpty(hashtag) || getHashtagsArray(hashtag).every((textHashtag) => textHashtag.startsWith('#'));
 pristine.addValidator(textHashtags, checkHashSign, 'Хэш-тег должен начинаться с символа # (решётка).');
 
 const checkHashLength = (hashtag) => getHashtagsArray(hashtag).every((textHashtag) => textHashtag.length <= MAX_LENGTH_HASHTAG);
@@ -36,12 +42,17 @@ const checkRepeatHashtags = (hashtag) => {
     return false;
   });
 };
+
 pristine.addValidator(textHashtags, checkRepeatHashtags, 'Такой хэштег уже существует.');
 
 const checkCommentLength = (comment) => checkTextLength(comment, MAX_LENGTH_COMMENT);
+
 pristine.addValidator(textDescription, checkCommentLength, `Длина комментария не может составлять больше ${MAX_LENGTH_COMMENT} символов`);
 
-imgUploadForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
-});
+
+const validateFormUpload = (evt) => {
+  if (!pristine.validate()) {
+    evt.preventDefault();}
+};
+
+export { imgUploadForm , textHashtags, textDescription, validateFormUpload, pristine};
